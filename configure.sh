20c64f4250df5aa4b1aff07dae40a8e6337caca5
Omar Alterkait <omal8805@colorado.edu>
2022-08-29 20:56:25 -0400

    made files machine independant

--- configure.sh
+++ configure.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
+HOMEDECAY=$PWD
 
-export LD_LIBRARY_PATH=/home/oalterkait/ubdecay/buildnuSQuIDS/lib:/home/oalterkait/ubdecay/buildSQuIDS/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=/home/oalterkait/ubdecay/buildnuSQuIDS/lib/pkgconfig:/home/oalterkait/ubdecay/buildSQuIDS/lib/pkgconfig:$PKG_CONFIG_PATH
-
-export PKG_CONFIG_PATH=/home/oalterkait/ubdecay/hdf5/HDF5-1.12.2-Linux/HDF_Group/HDF5/1.12.2/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=/home/oalterkait/ubdecay/hdf5/HDF5-1.12.2-Linux/HDF_Group/HDF5/1.12.2/lib:$LD_LIBRARY_PATH
-
-export NUSQUIDS_DATA_PATH=/home/oalterkait/ubdecay/buildnuSQuIDS/share/nuSQuIDS
-export LD_LIBRARY_PATH=/home/oalterkait/ubdecay/buildnuSQuIDS/lib:/home/oalterkait/ubdecay/buildSQuIDS/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/oalterkait/ubdecay/hdf5/HDF5-1.12.2-Linux/HDF_Group/HDF5/1.12.2/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=/home/oalterkait/ubdecay/buildnuSQuIDS/lib/pkgconfig:/home/oalterkait/ubdecay/buildSQuIDS/lib/pkgconfig:$PKG_CONFIG_PATH
-export NUSQUIDS_DATA_PATH=/home/oalterkait/ubdecay/buildnuSQuIDS/share/nuSQuIDS
+export PKG_CONFIG_PATH="${HOMEDECAY}/hdf5/HDF5-1.12.2-Linux/HDF_Group/HDF5/1.12.2/lib/pkgconfig:$PKG_CONFIG_PATH"
+export NUSQUIDS_DATA_PATH="${HOMEDECAY}/buildnuSQuIDS/share/nuSQuIDS"
+export LD_LIBRARY_PATH="${HOMEDECAY}/buildnuSQuIDS/lib:${HOMEDECAY}/buildSQuIDS/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="${HOMEDECAY}/hdf5/HDF5-1.12.2-Linux/HDF_Group/HDF5/1.12.2/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="${HOMEDECAY}/buildnuSQuIDS/lib/pkgconfig:${HOMEDECAY}/buildSQuIDS/lib/pkgconfig:$PKG_CONFIG_PATH"
+export NUSQUIDS_DATA_PATH="${HOMEDECAY}/buildnuSQuIDS/share/nuSQuIDS"
